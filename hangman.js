64a13fc6210a671faf34851753ea7fad861d48dc
tyler-radke1 <109247761+tyler-radke1@users.noreply.github.com>
2023-07-14 17:07:16 -0600

    initial commit

---
+++ hangman.js
@@ -0,0 +1,4 @@
+var words = ['apple', 'beach', 'chair', 'dance', 'earth', 'fence', 'grape', 'haste', 'image', 'jelly', 'knees', 'lamps', 'mango', 'nurse', 'ocean'];
+
+
+console.print("Hello World")
