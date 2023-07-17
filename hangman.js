0ccce18c653334783feed9a928dfbe6803e96375
tyler-radke1 <109247761+tyler-radke1@users.noreply.github.com>
2023-07-17 13:43:37 -0600

    stupid thing doesn't work

--- hangman.js
+++ hangman.js
@@ -1,4 +1,81 @@
 var words = ['apple', 'beach', 'chair', 'dance', 'earth', 'fence', 'grape', 'haste', 'image', 'jelly', 'knees', 'lamps', 'mango', 'nurse', 'ocean'];
 
+var randomWord = words[Math.floor(Math.random() * words.length)];
 
-console.print("Hello World")
+var gameIsOver = false;
+
+var textBox = document.getElementById("textBox");
+
+var submitButton = document.getElementById("submit");
+
+var blankWordInitial = "_ _ _ _ _"
+
+console.log(randomWord)
+
+function checkGuess() {
+    var word = document.getElementById('textBox').value.toString()
+
+    for(var i = 0; i < word.length; i++) {
+        const char = word[i]
+        if (randomWord.includes(char)) {
+            //Letter is included and needs to be added
+            letterIsIncluded(char)
+        } else {
+            letterIsNotIncluded(char)
+            //Letter is not included and needs to be added to failed guesses
+        }
+    }
+    
+ };
+
+ function letterIsIncluded(character) {
+    var char = character
+    
+    //Get reference to current word status 
+    //var currentWord = document.getElementById('word-lines').innerHTML
+    
+    //Get a copy that removes spaces 
+
+    blankWordInitial = removeSpaces(blankWordInitial)
+
+    //Iterate through, if randomWord[i] == char and currentWord[i] == _, change it to char 
+
+    for(var i = 0; i < randomWord.length; i++) {
+        if (randomWord[i] == char) {
+            blankWordInitial[i] = char
+        } else {
+            continue
+        }
+    }
+
+    //Separate with a space 
+    console.log(blankWordInitial)
+    blankWordInitial = addSpaces(blankWordInitial)
+    console.log(blankWordInitial)
+    document.getElementById("word-lines").innerHTML = blankWordInitial
+
+ }
+
+ function letterIsNotIncluded(character) {
+
+ }
+
+ function removeSpaces(str) {
+    if (typeof str == 'string') {
+        return str.split(' ').join('')
+    } else {
+        console.log("Not a string")
+    }
+    
+ }
+ function addSpaces(str) {
+    if (typeof str == "string") {
+        return str.split('').join(' ');
+    }
+    
+  }
+
+ 
+submitButton.addEventListener('click', checkGuess);
+
+
